eab538c7d8ec3c3cd332afe1849e0d423efbd247
johnnyteck <drillsjohnny00@gmail.com>
2022-07-06 15:25:52 +0100

    commit

--- index.js
+++ index.js
@@ -19,39 +19,41 @@ const endGame = document.getElementById("end-game")
 homeBtn1.addEventListener('click', function() {
     score1 += 1
     home.textContent = score1
- })
+})
  
 homeBtn2.addEventListener('click', function() {
     score1 += 2
     home.textContent = score1
- })
+})
 
 homeBtn3.addEventListener('click', function() {
     score1 += 3
     home.textContent = score1
- })
+})
  
  
- awayBtn1.addEventListener('click', function() {
+awayBtn1.addEventListener('click', function() {
     score2 += 1
     away.textContent = score2
- })
+})
  
- awayBtn2.addEventListener('click', function() {
+awayBtn2.addEventListener('click', function() {
     score2 += 2
     away.textContent = score2
- })
+})
 
- awayBtn3.addEventListener('click', function() {
+awayBtn3.addEventListener('click', function() {
     score2 += 3
     away.textContent = score2
- })
+})
 
 endGame.addEventListener('click', function() {
     if (score1 > score2) {
         comment.textContent = "HOME TEAM WINS"
-    } else if (score1 === score2) {
+    } else if (score1 == 0 && score2 == 0) {
         comment.textContent = "Match has been Cancelled"
+    } else if (score1 > 0 && score2 > 0 && score1 === score2) {
+        comment.textContent = "The Match is a Draw"
     }
     else {
         comment.textContent = "AWAY TEAM WINS"
